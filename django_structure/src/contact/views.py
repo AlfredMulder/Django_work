afc98b787dc14a2217a445cb77ffc16a01648e0a
AlfredMulder <xanderhref@protonmail.com>
2018-03-13 22:48:16 +0300

    Исправление форм.

--- django_structure/src/contact/views.py
+++ django_structure/src/contact/views.py
@@ -6,7 +6,7 @@ def contact(request):
     form = contactForm(request.POST or None)
 
     if dorm.is_valid():
-        print request.POST
+        print (request.POST)
     context = locals()
     template = 'contact.html'
     return render(request, template, context)
--- django_structure/src/django_structure/urls.py
+++ django_structure/src/django_structure/urls.py
@@ -28,7 +28,7 @@ urlpatterns = [
     path('about/', profiles_views.about, name='about'),
     path('contact/', contact_views.contact, name='contact'),
 ]
-#Добавление статических и медиа файлов при наличии соответствующих настроек.
+#Добавление статичных и медиа файлов при наличии соответствующих настроек.
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
