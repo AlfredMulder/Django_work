058fd2e58a943030248014bba75baf1e5a112375
AlfredMulder <xanderhref@protonmail.com>
2018-03-13 22:40:18 +0300

    Добавлены формы.

--- django_structure/src/contact/views.py
+++ django_structure/src/contact/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 
+from .forms import contactForm
 # Create your views here.
 def contact(request):
+    form = contactForm(request.POST or None)
+
+    if dorm.is_valid():
+        print request.POST
     context = locals()
     template = 'contact.html'
     return render(request, template, context)
