5dea255d8cadc3e93c8e10d075f60db03e6005d2
AlfredMulder <xanderhref@protonmail.com>
2018-03-21 23:09:46 +0300

    Попытка настроить heroku.

--- django_structure/src/django_structure/settings.py
+++ django_structure/src/django_structure/settings.py
@@ -149,7 +149,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
+
+
+
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, "static"),
+    ]
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 # Указание путей нахождения медиа и статических файлов:
 if DEBUG:
     MEDIA_URL = '/media/'
