9eafecdad7daef35d324472919e377e9fa12c09b
AlfredMulder <xanderhref@protonmail.com>
2018-03-24 01:18:29 +0300

    Settings.py

--- django_structure/src/django_structure/settings.py
+++ django_structure/src/django_structure/settings.py
@@ -41,7 +41,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'custom_user',
     'profiles',
     'contact',
     'crispy_forms',
