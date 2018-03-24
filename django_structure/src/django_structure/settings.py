1e508b848aa718b06a02fdbf44cf835f6103b485
AlfredMulder <xanderhref@protonmail.com>
2018-03-24 16:06:45 +0300

    Добавление базы данных postgresql.

--- django_structure/src/django_structure/settings.py
+++ django_structure/src/django_structure/settings.py
@@ -108,8 +108,12 @@ WSGI_APPLICATION = 'django_structure.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'db',
+        'USER': 'joe',
+        'PASSWORD': '201002b',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
